675abda2b0c8af8c47bcb850c6b1806d4d79ce09
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-11 16:22:09 +0200

    init

--- HowToCreateSimpleAnimation/ContentView.swift
+++ HowToCreateSimpleAnimation/ContentView.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show = false
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("SwiftUI")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color.pink)
+                .padding(4)
+                .animation(.easeOut(duration: 0.3))
+            
+            Image("img6")
+                .frame(width: show ? 414 : 300, height: show ? 300 : 300)
+                .clipped()
+                .cornerRadius(show ? 0 : 300)
+                .shadow(radius: show ? 0 : 30)
+                .blur(radius: show ? 0 : 30)
+                .animation(.spring())
+            
+            Text("Simple Animation in SwiftUI")
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundColor(Color.green)
+                .scaleEffect(show ? 2 : 1)
+                .padding(4)
+                .animation(.easeOut(duration: 0.3))
+            
+            Button(action: {
+                withAnimation {
+                    self.show.toggle()
+                }
+            }) {
+                Text("Animate")
+                    .colorMultiply(Color.green)
+            }
+            
+        }
     }
 }
 
